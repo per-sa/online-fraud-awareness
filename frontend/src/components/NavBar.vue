a327784dec35b6a2c4a839d68076af5458a9dbdb
clmb <ccolombomatheus@gmail.com>
2022-11-26 15:08:17 +0000

    Feat: Bootstrap and overall config

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">OFA - Online Fraud Awareness</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item" v-bind:key="i" v-for="i in links">
+                        <a class="nav-link" :href="`${i.path}`">{{ i.name }}</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+</template>
+
+
+<script>
+export default {
+    name: "NavBar",
+    data() {
+        return {
+            links: [
+                { name: "About", path: "/about" },
+                { name: "Safety Tools", path: "/safetytools" },
+                { name: "Contact", path: "/contact" },
+            ]
+        };
+    }
+};
+</script>
